42ec552eaa64faeea80511250eac118c4544fdc8
Jonathan Lacabe <jdlacabe@gmail.com>
2024-09-01 22:43:42 -0700

    Purge and switch entirely to java to make things easier.

---
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.testapp;
+import androidx.activity.ComponentActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ComponentActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState) { super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
