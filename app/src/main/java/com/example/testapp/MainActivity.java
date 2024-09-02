adbf52d66c624a89cb99068696058404c865173a
Jonathan Lacabe <jdlacabe@gmail.com>
2024-09-02 13:37:18 -0700

    multiple pages, landscape, trademark, night

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -31,29 +31,6 @@ public class MainActivity extends ComponentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtHello = findViewById(R.id.txtHello);
-        txtHello.setText(getString(R.string.hello));
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.settings_menu){
-            Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show();
-            return true;
-        }else if(item.getItemId() == R.id.alarm_menu){
-            Toast.makeText(this, "Alarm Selected", Toast.LENGTH_SHORT).show();
-            return true;
-        }else{
-            return super.onOptionsItemSelected(item);
-        }
 
     }
 }
