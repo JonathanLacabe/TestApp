8886c22983214239b3baedcb9f3bd3ba89a1bbe1
Jonathan Lacabe <jdlacabe@gmail.com>
2024-09-02 12:05:09 -0700

    Languages, xml stuff, a main menu that exists out of virtue of being the main menu, this is the bomb

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.testapp;
 import androidx.activity.ComponentActivity;
+import androidx.annotation.NonNull;
+
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,67 +24,36 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends ComponentActivity {
-    private ListView citiesList;
-    private Spinner studentsSpinner;
+    private TextView txtHello;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        citiesList = findViewById(R.id.citiesList);
-        studentsSpinner = findViewById(R.id.studentsSpinner);
+        txtHello = findViewById(R.id.txtHello);
+        txtHello.setText(getString(R.string.hello));
 
-        //Now, the data has been passed aesthetically using strings.xml instead of dynamically in here
+    }
 
-//        final ArrayList<String> students = new ArrayList<>();
-//        students.add("Mario");
-//        students.add("Brad");
-//        students.add("Sarah");
-//        students.add("Madeline");
-//        students.add("Tom");
-//
-//        ArrayAdapter<String> studentsAdapter = new ArrayAdapter<>(
-//                this, android.R.layout.simple_spinner_dropdown_item,
-//                students
-//        );
-//        studentsSpinner.setAdapter(studentsAdapter);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
-        studentsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, studentsSpinner.getSelectedItem().toString() + " Selected", Toast.LENGTH_SHORT).show();
-            }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.settings_menu){
+            Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if(item.getItemId() == R.id.alarm_menu){
+            Toast.makeText(this, "Alarm Selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }else{
+            return super.onOptionsItemSelected(item);
+        }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-
-
-        //Although we cannot change the object iself after making it final, we can use all of the functions within the object.
-        final ArrayList<String> cities = new ArrayList<>();
-        cities.add("Zurich");
-        cities.add("New York");
-        cities.add("Berlin");
-        cities.add("Moscow");
-        cities.add("Madrid");
-
-        ArrayAdapter<String> citiesAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                cities
-
-        );
-        citiesList.setAdapter(citiesAdapter);
-
-        citiesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, cities.get(position) + " Selected", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
